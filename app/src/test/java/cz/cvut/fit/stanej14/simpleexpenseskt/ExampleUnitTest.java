c0e46c5cd086012507bc971e998563d7478b6892
Jan Stanek <jan.stanek@ackee.cz>
2015-11-21 15:04:31 +0100

    initial commit

---
+++ app/src/test/java/cz/cvut/fit/stanej14/simpleexpenseskt/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package cz.cvut.fit.stanej14.simpleexpenseskt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
